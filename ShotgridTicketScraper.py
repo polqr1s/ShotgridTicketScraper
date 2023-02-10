10071bc6f0c828be238c22a55f1c656b6e41cddf
polqr1s <36319624+polqr1s@users.noreply.github.com>
2023-02-09 18:31:12 -0800

    Used regex to filter file paths from tickets

--- ShotgridTicketScraper.py
+++ ShotgridTicketScraper.py
@@ -1,24 +1,33 @@
-
-
 import shotgun_api3
-#studio_globals will need to replaced with your studio's global module that contains your entities
+# studio_globals will need to replaced with your studio's global module that contains your entities
 import studio_globals
+import re
 
-#the following will need to be replaced with your shotgrid connection info
+# the following will need to be replaced with your shotgrid connection info
 SG = Shotgun('https://my-site.shotgrid.autodesk.com', 'script_name', '0123456789abcdef0123456789abcdef0123456')
 
 def scrape():
-    ticketLink = (input('Enter the ticket URL:'))
     ticketNumber = int(ticketLink.split('/')[-1])
 
     filters = [['id', 'is', ticketNumber]]
     fields = ['title', 'description']
     result = SG.find_one(studio_globals.TICKET, filters, fields)
 
-    print('Title: \n' + result['title'])
-    print('Ticket ID: \n' + str(result['id']))
-    print('Description: \n' + result['description'])
+    print('TICKET ID INPUTTED: ' + str(result['id']))
+    print('TICKET TITLE: ' + result['title'])
 
-    scrape()
+    return result
 
-scrape()
+ticket = (input('Enter the ticket URL:'))
+data = scrape(ticket)
+
+# use below to print the entire ticket description for testing
+# print('\nFull Description: \n' + data['description'])
+
+slashList = ["\\", "//", "/"]
+if any(slashes in data['description'] for slashes in slashList):
+    print('\nPaths were found:')
+    filter = re.findall('\\\\.*', data['description'])
+    print(*filter, sep = "\n")
+else:
+    print('No paths found')
