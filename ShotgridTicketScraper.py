1a95410d0b7f55d90d814300271b96a71f79cb31
polqr1s <36319624+polqr1s@users.noreply.github.com>
2023-02-10 15:22:40 -0800

    Function to convert paths to zfs Isilon format

--- ShotgridTicketScraper.py
+++ ShotgridTicketScraper.py
@@ -2,12 +2,13 @@ import shotgun_api3
 # studio_globals will need to replaced with your studio's global module that contains your entities
 import studio_globals
 import re
+import pyperclip
 
 # the following will need to be replaced with your shotgrid connection info
 SG = Shotgun('https://my-site.shotgrid.autodesk.com', 'script_name', '0123456789abcdef0123456789abcdef0123456')
 
 def scrape():
-    ticketNumber = int(ticketLink.split('/')[-1])
+    ticketNumber = int(re.search('[0-9]+', ticketLink).group(0))
 
     filters = [['id', 'is', ticketNumber]]
     fields = ['title', 'description']
@@ -18,16 +19,36 @@ def scrape():
 
     return result
 
-ticket = (input('Enter the ticket URL:'))
-data = scrape(ticket)
+# Takes the ticket entity from scrape(), then finds and returns any file paths
+def filter(raw_ticket):
+    data = scrape(raw_ticket)
+    slashList = ["\\", "//", "/"]
+    if any(slashes in data['description'] for slashes in slashList):
+        print('\nPaths were found')
+        filteredPaths = re.findall('\\\\.*|//.*', data['description'])
+        organizedPaths = '\n'.join(filteredPaths)
+        return organizedPaths
+    else:
+        print('No paths found')
 
-# use below to print the entire ticket description for testing
-# print('\nFull Description: \n' + data['description'])
+# Takes the file paths from filter(), and converts them to zsh format for Isilon
+def convert():
+    ticketURL = (input('Enter the ticket URL:'))
+    paths = filter(ticketURL)
+    print(paths)
 
-slashList = ["\\", "//", "/"]
-if any(slashes in data['description'] for slashes in slashList):
-    print('\nPaths were found:')
-    filter = re.findall('\\\\.*', data['description'])
-    print(*filter, sep = "\n")
-else:
-    print('No paths found')
+    storageServer = input("Enter the storage server this data belongs to:")
+    if storageServer not in paths:
+        print("Warning: " + storageServer + " was not found in one or more paths. Check the paths and try again")
+        convert()
+    flipSlashes = paths.replace("\\", "/")
+    addIfs = flipSlashes.replace(storageServer, "ifs")
+    final = addIfs.replace("//", "/")
+
+    print("\n \n" + final)
+    pyperclip.copy("\n \n" + final)
+    print("The converted output has been copied to your clipboard")
+    print("Delete this data on " + storageServer)
+    convert()
+
+convert()
